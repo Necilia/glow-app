feb33eedb78b7cecaabf3736892708c5c061b6a7
Necilia <168139179+Necilia@users.noreply.github.com>
2026-06-16 14:19:07 -0300

    feat: add completed state to habit cards

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ setTimeout(() => {
     }, 1000)
 }, 2000)
 
+const dashboard = document.getElementById("dashboard")
+
+setTimeout(() => {
+    dashboard.style.display = "flex"
+    dashboard.style.animation = "fadeIn 1s ease-in-out forwards"
+}, 5000)
+
+const intro = document.getElementById("intro")
+
+setTimeout(() => {
+    intro.style.animation = "fadeOut 1s ease-in-out forwards"
+}, 4000)
+
+
+const cards = document.querySelectorAll(".card")
+
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("completed")
+    });
+});
+
